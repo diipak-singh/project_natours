88bca04729c0c98b434f7023ee389c2ff0019719
diipak-singh <deepaksinghbaise@gmail.com>
2020-05-07 10:56:18 +0530

    created and tested models to save data into database.

--- server.js
+++ server.js
@@ -13,12 +13,44 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false ,
+    useFindAndModify: false,
     useUnifiedTopology: true
   })
   .then(con => {
-      console.log(con.connections);
-      console.log('DB connection successful.')
+    console.log('DB connection successful.');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('Error is: ', err);
   });
 
 const port = process.env.PORT || 3000;
